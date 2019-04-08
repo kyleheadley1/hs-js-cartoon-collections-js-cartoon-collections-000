6f5b51f539d9c059310a0f267af3b0b9c90cb2af
Kyle Headley <kyleantonioheadley@gmail.com>
2019-04-08 23:34:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function dwarfRollCall(dwarves) {
-}
+  let result = ''
+  for(let i=0; i<dwarves.length; i=i+1){
+    let line = `${i + 1}. ${array[i]} `
+     result = result + line 
+  }
+console.log(result)
+dwarfRollCall(["Doc", "Dopey", "Bashful", "Grumpy"])
 
 function summonCaptainPlanet(planeteerCalls){
 }
